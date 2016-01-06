970b309319468c519d228ddad98899708d14fd21
Matthew Wiseman <wiser.man1@gmail.com>
2016-01-06 10:55:45 -0800

    Using temp var

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Matt
+ * Date: 2016-01-06
+ * Time: 10:50 AM
+ */
+
+$temp = 'Jim';
+echo 'Hi my name is';
+echo $temp;
+$temp = 'geek';
+echo "I am a";
+echo $temp;
+$temp = 10;
+echo 'My level is';
+echo $temp;
